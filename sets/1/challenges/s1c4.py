1157734f291b9c9f6f073e7799f909027ba98938
Varun Khaneja <ca.aawc@yahoo.com>
2017-07-10 02:45:43 -0700

    s/main/SingleCharacterXOR/

--- sets/1/challenges/s1c4.py
+++ sets/1/challenges/s1c4.py
@@ -6,11 +6,11 @@ def ReadLines(filename):
     content = [x.rstrip() for x in f.readlines()]
   return content
 
-def main(filename):
+def SingleCharacterXOR(filename):
   decoded = [sad.ScoreAndDecode(line)[1] for line in ReadLines(filename)]
   scores = [sad.EnglishLikeScore(l) for l in decoded]
   max_index = scores.index(max(scores))
   return max_index, scores[max_index], decoded[max_index]
 
 if __name__ == "__main__":
-  print main("4.txt")
+  print SingleCharacterXOR("4.txt")
